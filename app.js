c4806a96b0cd2a7d83d1d0fd5bc720fb2532f96c
phanthaoIT <ptthao.itus@gmail.com>
2018-12-12 12:41:53 +0700

    Add sequence and total money

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const app = express();
-
 require('./config/express')(app, logger, express, cookieParser, path);
 require('./config/mongoose')
 require('./routes')(app);
--- routes/detail.js
+++ routes/detail.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const GetByAddress = require('../controllers/GetByAddress')
+const coutMoney = require('../controllers/countMoney')
+const getSequence = require('../controllers/getSequence')
 router.get('/:address/list', async (req, res) => {
   let page = req.query.page || 1;
   let perpage = req.query.perpage || 20;
@@ -16,5 +18,26 @@ router.get('/:address/list', async (req, res) => {
     res.end();
   }
 })
-
+router.get('/:address/total',async (req,res)=>{
+  try {
+    let total = await coutMoney(req.params.address)
+    return res.json({
+      Total: total
+    })
+  } catch (e) {
+    res.status(400);
+    res.end();
+  }
+})
+router.get('/:address/sequence',async (req,res)=>{
+  try {
+    let sequence = await getSequence(req.params.address)
+    return res.json({
+      Sequence: sequence
+    })
+  } catch (e) {
+    res.status(400);
+    res.end();
+  }
+})
 module.exports = router;
